32cc81edf973dc6b2f58f36b2f47166bb8da957b
michael-mb <mbonimichael@yahoo.fr>
2023-09-29 20:04:23 +0200

    5.1.1 Planttext

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the output directory
+output_directory="/Users/michael/Documents/uni/4.Semester/Masterthesis/out"
+
+# Compile the LaTeX document to generate .bcf file
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory="$output_directory" main.tex
+
+# Run biber to process bibliography
+biber --input-directory="/Users/michael/Documents/uni/4.Semester/Masterthesis/out" main
+
+# Run makeglossaries to process glossaries
+makeglossaries main
+
+# Compile the LaTeX document again to resolve references
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory="$output_directory" main.tex
